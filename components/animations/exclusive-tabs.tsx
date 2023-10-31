9907968bb1b05681c04babf10b263dc38a555151
saisanthoshpentakota <saisanthosh.pentakota@contentstack.com>
2023-11-01 04:55:36 +0530

    MA-001 | Finish exclusive tabs components

--- components/animations/exclusive-tabs.tsx
+++ components/animations/exclusive-tabs.tsx
@@ -1,6 +1,10 @@
 'use client';
 
-import { useState } from 'react';
+import {
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 
 import { motion } from 'framer-motion';
 
@@ -19,7 +23,44 @@ const tabs: Tab[] = [
   { id: 5, label: 'Blog' },
 ];
 
-function ExclusiveTabs({ animationSpeed }: { animationSpeed?: number }) {
+function ExclusiveTabsUsingMixBlendMode({ animationSpeed }: { animationSpeed?: number }) {
+  let [activeTab, setActiveTab] = useState<number>(tabs[0].id);
+
+  return (
+    <div className='flex space-x-2 text-sm'>
+      {tabs.map((tab) => (
+        <button
+          key={tab.id}
+          onClick={() => {
+            setActiveTab(tab.id);
+          }}
+          className={cn(
+            'relative rounded-full px-3 py-1.5 font-medium outline-2 outline-orange-500',
+            // activeTab === tab.id && 'text-orange-00'
+          )}
+        >
+          {activeTab === tab.id && (
+            <motion.div
+              style={{
+                borderRadius: 9999,
+              }}
+              transition={{ duration: animationSpeed || 0.8 }}
+              layoutId='active-pill-blend-mode'
+              className='bg-orange-500 absolute inset-0'
+            ></motion.div>
+          )}
+          <span className='relative z-10 mix-blend-exclusion'>{tab.label}</span>
+        </button>
+      ))}
+    </div>
+  );
+}
+
+function ExclusiveTabsUsingBgBlendMode({
+  animationSpeed,
+}: {
+  animationSpeed?: number;
+}) {
   let [activeTab, setActiveTab] = useState<number>(tabs[0].id);
 
   return (
@@ -40,7 +81,7 @@ function ExclusiveTabs({ animationSpeed }: { animationSpeed?: number }) {
               style={{
                 borderRadius: 9999,
               }}
-              transition={{ type: 'spring', duration: animationSpeed || 0.8 }}
+              transition={{ duration: animationSpeed || 0.8 }}
               layoutId='active-pill-blend-mode'
               className='bg-orange-500 absolute inset-0'
             ></motion.div>
@@ -52,37 +93,99 @@ function ExclusiveTabs({ animationSpeed }: { animationSpeed?: number }) {
   );
 }
 
-function ExclusiveTabsUsingClipPath() {
+function ExclusiveTabsUsingClipPath({
+  animationSpeed,
+}: {
+  animationSpeed?: number;
+  }) {
+  let [mount, setIsMount] = useState<boolean>(false);
   let [activeTab, setActiveTab] = useState<number>(tabs[0].id);
+  const [tabDimensions, setTabDimensions] = useState({ width: 0, left: 0 });
+
+  useEffect(() => {
+    if (!mount) {
+      setIsMount(true)
+    }
+  },[])
+
+  // Define the type of elements the ref will hold
+  const tabsRef = useRef<HTMLButtonElement[]>([]);
+
+  const selectTab = (index: number) => {
+    setActiveTab(index);
+    if (tabsRef.current[index]) {
+      setTabDimensions({
+        width: tabsRef.current[index].offsetWidth,
+        left: tabsRef.current[index].offsetLeft,
+      });
+    }
+    console.log({tabsRef})
+  };
+
+  useEffect(() => {
+    if (tabsRef.current[activeTab]) {
+      setTabDimensions({
+        width: tabsRef.current[activeTab].offsetWidth,
+        left: tabsRef.current[activeTab].offsetLeft,
+      });
+    }
+  }, [activeTab]);
+
+  const clipPathStyle = {
+    clipPath: `inset(0px calc(100% - (${tabDimensions.left}px + ${tabDimensions.width}px)) calc(100% - (0px + 32px)) ${tabDimensions.left}px round 999px)`,
+  };
+
+  const transitionDuration = animationSpeed ? (animationSpeed * 1000).toString() + "ms" : "800ms"
 
   return (
-    <div className='flex space-x-2 text-sm'>
-      {tabs.map((tab) => (
-        <button
-          key={tab.id}
-          onClick={() => {
-            setActiveTab(tab.id);
-          }}
-          className={cn(
-            'relative rounded-full px-3 py-1.5 font-medium outline-2 outline-orange-500',
-            activeTab === tab.id && 'text-black'
-          )}
-        >
-          {activeTab === tab.id && (
-            <motion.div
-              style={{
-                borderRadius: 9999,
-              }}
-              transition={{ type: 'spring', duration: 0.8 }}
-              layoutId='active-pill'
-              className='bg-orange-500 absolute inset-0'
-            ></motion.div>
-          )}
-          <span className='relative z-10 bg-blend-exclusion'>{tab.label}</span>
-        </button>
-      ))}
+    <div className='flex text-sm relative'>
+      <div className='relative space-x-2 text-sm flex'>
+        {tabs.map((tab, index) => (
+          <button
+            ref={(el) => (tabsRef.current[tab.id] = el as HTMLButtonElement)}
+            key={tab.id}
+            onClick={() => selectTab(tab.id)}
+            className={cn(
+              'relative rounded-full px-3 py-1.5 font-medium outline-2 outline-orange-500 text-white', !mount && index === 0 && "bg-orange-500 text-black"
+            )}
+          >
+            <span className='relative z-10 bg-blend-exclusion'>
+              {tab.label}
+            </span>
+          </button>
+        ))}
+      </div>
+
+      <div
+        aria-hidden='true'
+        style={{
+          borderRadius: 9999,
+          clipPath: mount && clipPathStyle.clipPath || "",
+          transitionDuration: transitionDuration,
+        }}
+        className={cn('z-10 absolute top-0 left-0 hidden text-sm space-x-2', mount && "block bg-orange-500")}
+      >
+        {tabs.map((tab) => (
+          <button
+            aria-hidden="true"
+            key={tab.id}
+            onClick={() => {
+              setActiveTab(tab.id);
+            }}
+            className={cn(
+              'relative rounded-full px-3 py-1.5 font-medium outline-2 outline-orange-500 text-black'
+            )}
+          >
+            <span className='relative z-10'>{tab.label}</span>
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
 
-export { ExclusiveTabs, ExclusiveTabsUsingClipPath };
+export {
+  ExclusiveTabsUsingBgBlendMode,
+  ExclusiveTabsUsingClipPath,
+  ExclusiveTabsUsingMixBlendMode,
+};
