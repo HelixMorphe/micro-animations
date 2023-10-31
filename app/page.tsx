1e0f1a39bfad4b0ac4780a131d9d12af30b56225
saisanthoshpentakota <saisanthosh.pentakota@contentstack.com>
2023-11-01 03:26:06 +0530

    MA-001 | Implement animation speed logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,28 @@
-import { ExclusiveTabs } from '@/components/animations/exclusive-tabs';
+import {
+  ExclusiveTabs,
+  ExclusiveTabsUsingClipPath,
+} from '@/components/animations/exclusive-tabs';
 import DisplayBox from '@/components/ui/display-box';
 
 export default function Home() {
   return (
     <main className='container'>
-      <div className='min-h-[calc(100vh-64px)] w-4/5 m-auto flex items-center '>
+      <div className='min-h-[calc(100vh-64px)] w-4/5 m-auto'>
+        <h2 className='pt-10 text-3xl font-semibold tracking-tight transition-colors'>
+          Exclusion tabs
+        </h2>
+        <h3 className='mt-8 mb-6 scroll-m-20 text-xl font-medium tracking-tight'>
+          using <span>mix-blend-mode</span>
+        </h3>
+        {/* <p className='mb-8 leading-7 [&:not(:first-child)]:mt-6'></p> */}
         <DisplayBox>
           <ExclusiveTabs />
+        </DisplayBox>
+        <h3 className='mt-8 mb-6 scroll-m-20 text-xl font-medium tracking-tight'>
+          using <span>clip-path</span>
+        </h3>
+        <DisplayBox>
+          <ExclusiveTabsUsingClipPath />
         </DisplayBox>
       </div>
     </main>
--- components/ui/display-box.tsx
+++ components/ui/display-box.tsx
@@ -1,4 +1,10 @@
-import React, { ReactNode } from 'react';
+'use client';
+
+import React, {
+  ReactElement,
+  useEffect,
+  useState,
+} from 'react';
 
 import {
   Select,
@@ -8,13 +14,37 @@ import {
   SelectValue,
 } from './select';
 
-const DisplayBox = ({ children }: { children?: ReactNode }) => {
+const DisplayBox = ({ children }: { children?: ReactElement }) => {
+  let [animationSpeed, setAnimationSpeed] = useState<number>(0);
+
+  const handleSelectChange = (value: string) => {
+    if (value === "1") {
+      setAnimationSpeed(0.8)
+    } else if (value === "2") {
+       setAnimationSpeed(0.2);
+    } else if (value === "3") {
+       setAnimationSpeed(5);
+    } else {
+      setAnimationSpeed(999)
+    }
+  };
+
+  const enhancedChildren = children
+    ? React.Children.map(children, (child) =>
+        React.cloneElement(child, { animationSpeed })
+      )
+    : children;
+
+  useEffect(() => {
+    console.log('value changed', animationSpeed);
+  }, [animationSpeed]);
+
   return (
     <div className='relative w-full border rounded-lg flex-col overflow-hidden'>
       <div className='absolute right-4 top-4'>
-        <Select>
+        <Select onValueChange={handleSelectChange}>
           <SelectTrigger>
-            <SelectValue placeholder="Animation Speed"/>
+            <SelectValue placeholder='Animation Speed' />
           </SelectTrigger>
           <SelectContent>
             <SelectItem value='2'>Fast</SelectItem>
@@ -24,7 +54,7 @@ const DisplayBox = ({ children }: { children?: ReactNode }) => {
         </Select>
       </div>
       <div className='h-[200px] bg-black/80 flex items-center justify-center'>
-        {children}
+        {enhancedChildren}
       </div>
     </div>
   );
