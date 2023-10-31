b15c1f97b84550cb3cc6cc2309946603f1d3a102
saisanthoshpentakota <saisanthosh.pentakota@contentstack.com>
2023-11-01 03:26:06 +0530

    MA-001 | Implement animation speed logic

--- components/animations/exclusive-tabs.tsx
+++ components/animations/exclusive-tabs.tsx
@@ -11,7 +11,7 @@ interface Tab {
   label: string;
 }
 
-let tabs: Tab[] = [
+const tabs: Tab[] = [
   { id: 1, label: 'Home' },
   { id: 2, label: 'Product' },
   { id: 3, label: 'Solution' },
@@ -19,7 +19,7 @@ let tabs: Tab[] = [
   { id: 5, label: 'Blog' },
 ];
 
-function ExclusiveTabs() {
+function ExclusiveTabs({ animationSpeed }: { animationSpeed?: number }) {
   let [activeTab, setActiveTab] = useState<number>(tabs[0].id);
 
   return (
@@ -40,7 +40,40 @@ function ExclusiveTabs() {
               style={{
                 borderRadius: 9999,
               }}
-              transition={{ type: 'spring', duration: 5 }}
+              transition={{ type: 'spring', duration: animationSpeed || 0.8 }}
+              layoutId='active-pill-blend-mode'
+              className='bg-orange-500 absolute inset-0'
+            ></motion.div>
+          )}
+          <span className='relative z-10 bg-blend-exclusion'>{tab.label}</span>
+        </button>
+      ))}
+    </div>
+  );
+}
+
+function ExclusiveTabsUsingClipPath() {
+  let [activeTab, setActiveTab] = useState<number>(tabs[0].id);
+
+  return (
+    <div className='flex space-x-2 text-sm'>
+      {tabs.map((tab) => (
+        <button
+          key={tab.id}
+          onClick={() => {
+            setActiveTab(tab.id);
+          }}
+          className={cn(
+            'relative rounded-full px-3 py-1.5 font-medium outline-2 outline-orange-500',
+            activeTab === tab.id && 'text-black'
+          )}
+        >
+          {activeTab === tab.id && (
+            <motion.div
+              style={{
+                borderRadius: 9999,
+              }}
+              transition={{ type: 'spring', duration: 0.8 }}
               layoutId='active-pill'
               className='bg-orange-500 absolute inset-0'
             ></motion.div>
@@ -52,4 +85,4 @@ function ExclusiveTabs() {
   );
 }
 
-export { ExclusiveTabs };
+export { ExclusiveTabs, ExclusiveTabsUsingClipPath };
